06741fb9c6883daeec7cb93617cae8acd9e83882
jiayuxin007 <44504046+jiayuxin007@users.noreply.github.com>
2026-03-14 01:19:23 +0100

    Update translation-helper.js

--- js/translation-helper.js
+++ js/translation-helper.js
@@ -15,7 +15,8 @@ class TranslationHelper {
             bravo: '好；能干；勇敢',
             cola: '可乐',
             acqua: '水',
-            ha: '有'
+            ha: '有',
+            zeta:'z'
         };
     }
 
